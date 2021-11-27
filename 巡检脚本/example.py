ac6e6f57f49c77c0953b0d10cf324501993a3dac
“wangzhenye2019” <wangzhenye2019@gmail.com>
2021-11-27 12:48:45 +0800

    增加巡检脚本目录

---
+++ 巡检脚本/example.py
@@ -0,0 +1,12 @@
+"""
+---------------------------------------
+# -*- coding: utf-8 -*-
+# @Time : 2021/11/27 12:48 下午
+# @Author : wangzhenye2019
+# @Project_name : Oracle
+# @Filename : example.py
+# @Software : PyCharm 
+# @Github : https://github.com/wangzhenye2019/
+---------------------------------------
+"""
+...
